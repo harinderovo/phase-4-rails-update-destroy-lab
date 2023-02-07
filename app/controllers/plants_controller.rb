d4bcd7a542aaebc3585ad931eccc5c52d06c33a6
Harinder Singh <harmiglany@yahoo.com>
2023-02-07 10:32:22 -0800

    commit tree 579de6ea1f6ce02640a6f720f3201fb0975d177b

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,4 +1,5 @@
 class PlantsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
   # GET /plants
   def index
@@ -16,6 +17,29 @@ class PlantsController < ApplicationController
   def create
     plant = Plant.create(plant_params)
     render json: plant, status: :created
+  end
+
+  # PATCH /plants/:id
+  def update
+    # plant = Plant.find(params[:id])
+    # render json: plant, status: :ok
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      plant.update(plant_params)
+      render json: plant
+    else 
+      render json: { error: "Plant not found" }, status: :not_found
+    end
+  end
+
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      plant.destroy
+      head :no_content
+    else
+      render json: { error: "Plant not found" }, status: :not_found
+    end
   end
 
   private
